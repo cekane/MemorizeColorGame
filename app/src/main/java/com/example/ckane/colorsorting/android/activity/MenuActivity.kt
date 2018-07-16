c7bbf9900c0249c5298acb15288e2f33f065dc1c
Connor <ckane144@gmail.com>
2018-07-15 21:57:43 -0400

    *Updated UI

--- app/src/main/java/com/example/ckane/colorsorting/android/activity/MenuActivity.kt
+++ app/src/main/java/com/example/ckane/colorsorting/android/activity/MenuActivity.kt
@@ -2,15 +2,11 @@ package com.example.ckane.colorsorting.android.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.ckane.colorsorting.R
-
-import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
